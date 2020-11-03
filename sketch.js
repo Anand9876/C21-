20c6235c6f6927c67fb7fafdcffc30b74b05f2be
Anand9876 <73025487+Anand9876@users.noreply.github.com>
2020-11-03 17:12:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,10 +20,10 @@ function draw() {
 		bullet.velocityX=0;
 		var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
 		if(damage>10){
-			wall.shapeColor=("green");
+			wall.shapeColor=color(255,0,0);
 		}
 		if(damage<10){
-			wall.shapeColor=("orange");
+			wall.shapeColor=color(0,255,0);
 		}
 
   }
