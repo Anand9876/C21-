4c05800e13d74ea53f13bbb6bd34993a45e8ddfb
Anand9876 <73025487+Anand9876@users.noreply.github.com>
2020-11-03 17:37:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function draw() {
   background(0); 
   bullet.velocityX=speed;
   if(hascollided(bullet,wall)) 
-  {
+  { 
 		bullet.velocityX=0;
 		var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
 		if(damage>10){
